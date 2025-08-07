b8439b47db01e996dfb5d739b168411f62f8e70f
Ivan Arasco <arascoivan97@gmail.com>
2025-08-07 15:20:56 +0200

    Gallery images from MUI

--- resources/js/pages/Journey.jsx
+++ resources/js/pages/Journey.jsx
@@ -3,10 +3,14 @@ import '../../sass/app.scss';
 
 import BlockHeader from ".././components/Block-header";
 
+import Gallery from ".././components/Mui-gallery";
+
 export default function Journey() {
   return (
     <>
       <BlockHeader title="Viajes" content="Contenido sobre mis viajes más destacables"/>
+
+      <Gallery />
     </>
   );
 }
